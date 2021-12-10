387a189ebb8fac1c14a0979786f6322ef110b756
Angelique Abacajan <ma_utsey@yahoo.com>
2021-12-10 12:44:15 -0500

    Created a reources table in migrations

--- data/migrations/20211210171811_first-migration.js
+++ data/migrations/20211210171811_first-migration.js
@@ -1,12 +1,23 @@
 
 exports.up = function(knex) {
   return knex.schema
+
+    // Projects
     .createTable("projects", (table) => {
         table.increments("project_id");
         table.string("project_name", 50).notNullable();
         table.string("project_description", 200);
         table.boolean("project_completed", false);
     })
+
+    // Resources
+    .createTable("resources", (table) => {
+        table.increments("resource_id");
+        table.string("resource_name", 50).unique().notNullable();
+        table.string("resource_description", 200);
+    })
+
+    
 };
 
 exports.down = function(knex) {
